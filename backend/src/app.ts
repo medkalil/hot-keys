a49619f8f85908f05cf0cb52c628b389ca9a8cf5
medkalil <medkalilbenahmed@gmail.com>
2026-07-11 10:39:46 +0100

    cors fix

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -9,7 +9,7 @@ const app: Express = express();
 
 // Middleware
 app.use(cors({
-  origin: ['http://localhost:5173', 'http://localhost:3000', 'http://127.0.0.1:5173', 'https://hot-keys.onrender.com/api/operators'],
+  origin: true,
   credentials: true,
 }));
 app.use(express.json());
